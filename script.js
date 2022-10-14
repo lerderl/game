8e6affbee5bec046ed1b32cd45600d5c8bc6820c
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-10-14 16:48:49 +0100

    Update script.js
    This commit will add function to play a single round of the game.

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 function computerPlay() {
   let options = ["rock", "paper", "scissors"];
-  computerSelection = options[Math.floor(Math.random() * options.length)];
-  console.log(computerSelection);
+  return options[Math.floor(Math.random() * options.length)];
 };
 
-computerPlay();
+function play(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "A tie";
+  }
+  else if (playerSelection == 'rock') {
+    if (computerSelection == 'paper') {
+      return `You Lose! Computer selection of ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You Win! ${playerSelection} beats computer selection of ${computerSelection}`;
+    }
+  }
+  else if (playerSelection == 'scissors') {
+    if(computerSelection == 'rock'){
+      return `You Lose! Computer selection of ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You Win! ${playerSelection} beats computer selection of ${computerSelection}`;
+    }
+  }
+  else if (playerSelection == 'paper') {
+    if(computerSelection == 'scissors'){
+      return `You Lose! Computer selection of ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You Win! ${playerSelection} beats computer selection of ${computerSelection}`;
+    }
+  }
+  else {
+    alert("Please enter the right option");
+  }
+};
+
+let playerSelection = window.prompt("Enter your option");
+let computerSelection = computerPlay();
+console.log(play(playerSelection, computerSelection));

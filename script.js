5814a5e0bc781e252a8fef876c23a1337f75ee9c
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-10-19 18:36:56 +0100

    Create buttons
    This commit will:
    Create three buttons, one for each selection.
    Add an event listener to the buttons that call play function with the correct playerSelection every time a button is clicked.

--- script.js
+++ script.js
@@ -41,35 +41,48 @@ function play(playerSelection, computerSelection) {
 };
 
 // Function to play all rounds of the game
-function game() {
-  for (let i = 0; i < 5; i++) {
-    console.log(`ROUND ${i + 1}`);
-    const playerOptions = ["rock", "paper", "scissors"];
-    let playerSelection = window.prompt("Enter your option");
-    if (!playerOptions.includes(playerSelection)) {
-      alert("Please enter the right option of either rock, paper or scissors");
-      i--;
-      continue;
-    }
-    let computerSelection = computerPlay();
-    console.log(play(playerSelection, computerSelection));
-  };
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(`ROUND ${i + 1}`);
+//     const playerOptions = ["rock", "paper", "scissors"];
+    // let playerSelection = window.prompt("Enter your option");
+    // if (!playerOptions.includes(playerSelection)) {
+    //   alert("Please enter the right option of either rock, paper or scissors");
+    //   i--;
+    //   continue;
+    // }
+    // let computerSelection = computerPlay();
+    // console.log(play(playerSelection, computerSelection));
+//   };
 
-  console.log(`You: ${playerScore}   Computer: ${computerScore}`);
+//   console.log(`You: ${playerScore}   Computer: ${computerScore}`);
 
-  while (playerScore === computerScore) {
-    console.log("It's a tie. Please play again");
-    let playerSelection = window.prompt("Enter your option");
-    let computerSelection = computerPlay();
-    console.log(play(playerSelection, computerSelection));
-    console.log(`You: ${playerScore}   Computer: ${computerScore}`);
-  };
+//   while (playerScore === computerScore) {
+//     console.log("It's a tie. Please play again");
+//     let playerSelection = window.prompt("Enter your option");
+//     let computerSelection = computerPlay();
+//     console.log(play(playerSelection, computerSelection));
+//     console.log(`You: ${playerScore}   Computer: ${computerScore}`);
+//   };
 
-  console.log("Thank you for playing");
-};
+//   console.log("Thank you for playing");
+// };
 
 
 let playerScore = 0,
 computerScore = 0;
 
-game();
+// game();
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    let playerSelection = button.id;
+    let computerSelection = computerPlay();
+    console.log(play(playerSelection, computerSelection));
+  })
+});
+
+// const playerResult = document.getElementById("playerScore");
+// console.log(playerResult);

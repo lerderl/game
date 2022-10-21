e9ac196de8994f16af71706db68dc91050457fb7
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-10-21 17:48:31 +0100

    Complete display
    This commit will display the running score, and announce a winner of the game once one player reaches 5
    points

--- script.js
+++ script.js
@@ -40,48 +40,27 @@ function play(playerSelection, computerSelection) {
   }
 };
 
-// Function to play all rounds of the game
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     console.log(`ROUND ${i + 1}`);
-//     const playerOptions = ["rock", "paper", "scissors"];
-    // let playerSelection = window.prompt("Enter your option");
-    // if (!playerOptions.includes(playerSelection)) {
-    //   alert("Please enter the right option of either rock, paper or scissors");
-    //   i--;
-    //   continue;
-    // }
-    // let computerSelection = computerPlay();
-    // console.log(play(playerSelection, computerSelection));
-//   };
-
-//   console.log(`You: ${playerScore}   Computer: ${computerScore}`);
-
-//   while (playerScore === computerScore) {
-//     console.log("It's a tie. Please play again");
-//     let playerSelection = window.prompt("Enter your option");
-//     let computerSelection = computerPlay();
-//     console.log(play(playerSelection, computerSelection));
-//     console.log(`You: ${playerScore}   Computer: ${computerScore}`);
-//   };
-
-//   console.log("Thank you for playing");
-// };
-
-
 let playerScore = 0,
 computerScore = 0;
-
-// game();
-
 const buttons = document.querySelectorAll('button');
 
+// Event listener for each button
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    let playerSelection = button.id;
-    let computerSelection = computerPlay();
-    play(playerSelection, computerSelection);
-    document.getElementById("playerScore").textContent = `${playerScore}`;
-    document.getElementById("computerScore").textContent = `${computerScore}`;
+    if (playerScore < 5 && computerScore < 5) {
+      let playerSelection = button.id;
+      let computerSelection = computerPlay();
+      play(playerSelection, computerSelection);
+      document.getElementById("playerScore").textContent = `${playerScore}`;
+      document.getElementById("computerScore").textContent = `${computerScore}`;
+    }
+    document.getElementById("result").textContent = `You: ${playerScore} Computer: ${computerScore}`;
+    if (playerScore == 5 || computerScore == 5) {
+      if (playerScore > computerScore) {
+        document.getElementById("final").textContent = `You win ${playerScore} to ${computerScore}`;      
+      } else if (computerScore > playerScore) {
+        document.getElementById("final").textContent = `Computer wins ${computerScore} to ${playerScore}`;      
+      }
+    }
   })
 });
